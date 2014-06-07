4ecfd394cdaed14b74a6b3bca895f6c8df7eb172
Keeper <phidias.chiang@gmail.com>
2014-06-07 18:07:15 +0800

    Looping implemented

--- main.go
+++ main.go
@@ -3,26 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	var input = ""
-	var buf [1024]byte
-	var ptr = 0
+	input := ""
+	buf := [30]byte{}
+	ptr := 0
+	rt_pc_stk := []int{}
+	pc := 0
 	fmt.Scanf("%s", &input)
-	for _, c := range input {
+	for pc != len(input) {
+		c := input[pc]
 		switch c {
 		case '>':
 			ptr++
+			pc++
 		case '<':
 			ptr--
+			pc++
 		case '+':
 			buf[ptr]++
+			pc++
 		case '-':
 			buf[ptr]--
+			pc++
 		case '.':
 			fmt.Printf("%c", buf[ptr])
+			pc++
 		case ',':
 			fmt.Scanf("%c", &buf[ptr])
-		case '[', ']':
-			fmt.Println("Not implemented")
+			pc++
+		case '[':
+			pc++
+			rt_pc_stk = append(rt_pc_stk, pc)
+		case ']':
+			rt_to := rt_pc_stk[len(rt_pc_stk)-1] // get top of the stack
+			if buf[ptr] != 0 {
+				pc = rt_to
+			} else {
+				rt_pc_stk = rt_pc_stk[:len(rt_pc_stk)-1] // pop the return pointer
+				pc++
+			}
 		}
 	}
 }
